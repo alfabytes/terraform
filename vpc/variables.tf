4b96466b23e94c9eee902a620703fabc3cc734e8
alfathmas <alfathmas24@gmail.com>
2024-08-04 00:22:20 +0700

    remove variable that unused

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -12,17 +12,6 @@ variable "vpc_cidr" {
   description = "VPC CIDR"
   default = "10.0.0.0/16"
 }
-
-variable "ami" {
-  description = "EC2 AMI"
-  default = "ami-0a0e5d9c7acc336f1"
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  default = "t2.micro"
-}
-
 variable "tags" {
   description = "Tags for the resources"
   type = map(string)
